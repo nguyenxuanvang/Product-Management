c78b8269d3fb6801d53a2a09b387869bc9432bec
nguyenxuanvang <nguyenvang14722@gmail.com>
2022-10-19 00:34:04 +0700

    Update file script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const inputNodes = document.querySelectorAll('.insert-form div input');
 const buttonNode = document.querySelector('.insert-form button');
+const bodyNode = document.querySelector('tbody');
+const dataListNode = document.querySelector('.data-list');
 let productLists = JSON.parse(localStorage.getItem('productLists'));
 if(productLists === null) productLists = [];
+if(productLists.length === 0) dataListNode.classList.add('hidden');
+else {
+    productLists.forEach(item => {
+        const trNode = document.createElement('tr');
+        bodyNode.appendChild(trNode);
+        trNode.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.cost}đ</td>
+            <td>
+                <a class="action-button edit-button" href="#">Edit</a>
+                <a class="action-button delete-button" href="#">Delete</a>
+                <button class="save-button hidden">Lưu</button>
+                <button class="cancel-button hidden">Quay Lại</button>
+            </td>
+        `;
+    });
+}
+let deleteButtons = document.querySelectorAll('.delete-button');
+let editButtons = document.querySelectorAll('.edit-button');
+let saveButtons = document.querySelectorAll('.save-button');
+let cancelButtons = document.querySelectorAll('.cancel-button');
 let kt = 0;
-const dataListNode = document.querySelector('.data-list');
 function Product(id,name,cost) {
     this.id = id;
     this.name = name;
@@ -32,6 +55,64 @@ const findIndex = function(array, index) {
             return i;
     return -1;
 }
+const deleteFunction = function(item) {
+    let choice = false;
+    let idChoice = item.parentElement.parentElement.children[0].textContent;
+    choice = confirm (`Bạn có muốn xóa sản phẩm (id=${idChoice})`);
+    if(choice === true) {
+        for(let i = 0; i < productLists.length; i += 1)
+            if(productLists[i].id === idChoice){
+                productLists.splice(i,1);
+                localStorage.setItem('productLists',JSON.stringify(productLists));
+                break;
+            }
+        const trNode = item.parentElement.parentElement;
+        trNode.remove();
+        if(productLists.length === 0) dataListNode.classList.add('hidden');
+    }
+}
+const editFunction = function(item) {
+    const trNode = item.parentElement.parentElement;
+    const idChoose = findIndex(productLists,trNode.children[0].textContent);
+    trNode.children[3].children[0].classList.toggle('hidden');
+    trNode.children[3].children[1].classList.toggle('hidden');
+    trNode.children[3].children[2].classList.toggle('hidden');
+    trNode.children[3].children[3].classList.toggle('hidden');
+    trNode.children[1].innerHTML = `
+    <input type="text" name="name" value="${productLists[idChoose].name}">
+    `;
+    trNode.children[2].innerHTML = `
+        <input type="text" name="price" value="${productLists[idChoose].cost}">
+    `;
+}
+const saveFuction = function(item) {
+    const trNode = item.parentElement.parentElement;
+    const idChoose = findIndex(productLists,trNode.children[0].textContent);
+    if(trNode.children[1].children[0].value === '' || trNode.children[2].children[0].value === '')
+        alert('Vui lòng không để trống');
+    else {
+        productLists[idChoose].name = trNode.children[1].children[0].value;
+        productLists[idChoose].cost = trNode.children[2].children[0].value;
+        trNode.children[3].children[0].classList.toggle('hidden');
+        trNode.children[3].children[1].classList.toggle('hidden');
+        trNode.children[3].children[2].classList.toggle('hidden');
+        trNode.children[3].children[3].classList.toggle('hidden');
+        trNode.children[1].innerHTML = `${productLists[idChoose].name}`;
+        trNode.children[2].innerHTML = `${productLists[idChoose].cost}đ`;
+        localStorage.setItem('productLists',JSON.stringify(productLists));
+        alert('Lưu thành công');
+    }
+}
+const cancelFunction = function(item) {
+    const trNode = item.parentElement.parentElement;
+    const idChoose = findIndex(productLists,trNode.children[0].textContent);
+    trNode.children[3].children[0].classList.toggle('hidden');
+    trNode.children[3].children[1].classList.toggle('hidden');
+    trNode.children[3].children[2].classList.toggle('hidden');
+    trNode.children[3].children[3].classList.toggle('hidden');
+    trNode.children[1].innerHTML = `${productLists[idChoose].name}`;
+    trNode.children[2].innerHTML = `${productLists[idChoose].cost}đ`;
+}
 buttonNode.addEventListener('click',() => {
    
     if(checkValid(inputNodes) === 1)
@@ -43,97 +124,67 @@ buttonNode.addEventListener('click',() => {
             productLists.push(product);
             localStorage.setItem('productLists',JSON.stringify(productLists));
             alert('Lưu thành công');
-            location.reload();
+            if(productLists.length !== 0) dataListNode.classList.remove('hidden');
+            const trNode = document.createElement('tr');
+            bodyNode.appendChild(trNode);
+            trNode.innerHTML = `
+                <td>${product.id}</td>
+                <td>${product.name}</td>
+                <td>${product.cost}đ</td>
+                <td>
+                    <a class="action-button edit-button" href="#">Edit</a>
+                    <a class="action-button delete-button" href="#">Delete</a>
+                    <button class="save-button hidden">Lưu</button>
+                    <button class="cancel-button hidden">Quay Lại</button>
+                </td>
+            `;
+            deleteButtons = document.querySelectorAll('.delete-button');
+            const deleteElement = deleteButtons[deleteButtons.length-1];
+            deleteElement.addEventListener('click',(event)=>{
+                event.preventDefault();
+                deleteFunction(deleteElement);
+            });
+            editButtons = document.querySelectorAll('.edit-button');
+            const editElement = editButtons[editButtons.length-1];
+            editElement.addEventListener('click',(event)=>{
+                event.preventDefault();
+                editFunction(editElement);
+            });
+            saveButtons = document.querySelectorAll('.save-button');
+            const saveElement = saveButtons[saveButtons.length-1];
+            saveElement.addEventListener('click',() => {
+                    saveFuction(saveElement);
+                });
+            cancelButtons = document.querySelectorAll('.cancel-button');
+            const cancelElement = cancelButtons[cancelButtons.length-1];
+            cancelElement.addEventListener('click',() => {
+                cancelFunction(cancelElement); 
+            });
+            inputNodes.forEach(item => {
+                item.value = '';
+            })
         }
     }
 });
-if(productLists.length === 0) dataListNode.classList.add('hidden');
-else {
-    const bodyNode = document.querySelector('tbody');
-    productLists.forEach(item => {
-        const trNode = document.createElement('tr');
-        bodyNode.appendChild(trNode);
-        trNode.innerHTML = `
-            <td>${item.id}</td>
-            <td>${item.name}</td>
-            <td>${item.cost}đ</td>
-            <td>
-                <a class="action-button edit-button" href="#">Edit</a>
-                <a class="action-button delete-button" href="#">Delete</a>
-                <button class="save-button hidden">Lưu</button>
-                <button class="cancel-button hidden">Quay Lại</button>
-            </td>
-        `;
-    });
-}
-const deleteButtons = document.querySelectorAll('.delete-button');
 deleteButtons.forEach(item => {
     item.addEventListener('click',(event)=> {
         event.preventDefault();
-        let choice = false;
-        let idChoice = event.target.parentElement.parentElement.children[0].textContent;
-        choice = confirm (`Bạn có muốn xóa sản phẩm (id=${idChoice})`);
-        if(choice === true) {
-            for(let i = 0; i < productLists.length; i += 1)
-                if(productLists[i].id === idChoice){
-                    productLists.splice(i,1);
-                    localStorage.setItem('productLists',JSON.stringify(productLists));
-                    break;
-                }
-            const trNode = item.parentElement.parentElement;
-            trNode.remove();
-            if(productLists.length === 0) location.reload();
-        }
+        deleteFunction(item);
     });
 });
-const editButtons = document.querySelectorAll('.edit-button');
 editButtons.forEach(item => {
-    item.addEventListener('click',()=>{
-        const trNode = item.parentElement.parentElement;
-        const idChoose = findIndex(productLists,trNode.children[0].textContent);
-        trNode.children[3].children[0].classList.toggle('hidden');
-        trNode.children[3].children[1].classList.toggle('hidden');
-        trNode.children[3].children[2].classList.toggle('hidden');
-        trNode.children[3].children[3].classList.toggle('hidden');
-        trNode.children[1].innerHTML = `
-        <input type="text" name="name" value="${productLists[idChoose].name}">
-        `;
-        trNode.children[2].innerHTML = `
-            <input type="text" name="price" value="${productLists[idChoose].cost}">
-        `;
+    item.addEventListener('click',(event)=>{
+        event.preventDefault();
+        editFunction(item);
     });
 });
-const saveButtons = document.querySelectorAll('.save-button');
 saveButtons.forEach(item => {
     item.addEventListener('click',() => {
-        const trNode = item.parentElement.parentElement;
-        const idChoose = findIndex(productLists,trNode.children[0].textContent);
-        if(trNode.children[1].children[0].value === '' || trNode.children[2].children[0].value === '')
-            alert('Vui lòng không để trống');
-        else {
-                productLists[idChoose].name = trNode.children[1].children[0].value;
-                productLists[idChoose].cost = trNode.children[2].children[0].value;
-                trNode.children[3].children[0].classList.toggle('hidden');
-                trNode.children[3].children[1].classList.toggle('hidden');
-                trNode.children[3].children[2].classList.toggle('hidden');
-                trNode.children[3].children[3].classList.toggle('hidden');
-                trNode.children[1].innerHTML = `${productLists[idChoose].name}`;
-                trNode.children[2].innerHTML = `${productLists[idChoose].cost}đ`;
-                localStorage.setItem('productLists',JSON.stringify(productLists));
-                alert('Lưu thành công');
-        }
+        saveFuction(item);
     });
 });
-const cancelButtons = document.querySelectorAll('.cancel-button');
 cancelButtons.forEach(item =>{
     item.addEventListener('click',() => {
-        const trNode = item.parentElement.parentElement;
-        const idChoose = findIndex(productLists,trNode.children[0].textContent);
-        trNode.children[3].children[0].classList.toggle('hidden');
-        trNode.children[3].children[1].classList.toggle('hidden');
-        trNode.children[3].children[2].classList.toggle('hidden');
-        trNode.children[3].children[3].classList.toggle('hidden');
-        trNode.children[1].innerHTML = `${productLists[idChoose].name}`;
-        trNode.children[2].innerHTML = `${productLists[idChoose].cost}đ`;
+       cancelFunction(item); 
     });
 });
